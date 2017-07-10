a23339c0ecabd2309c5c8166fb397d32050ef27b
L <L>
2017-07-10 14:25:47 +0100

    Add join page

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom(){
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight){
+    messages.scrollTop(scrollHeight);
+  };
+
+};
+
 socket.on('connect',function(){
   console.log('connected to server');
 
@@ -16,9 +32,7 @@ socket.on('newMessage',function(message){
     createdAt:message.createdAt,
   });
   $('#messages').append(html);
-  // var li = $('<li></li>');
-  // li.text(`${message.from} ${message.createdAt}: ${message.text}`);
-  // $('#messages').append(li);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage',function(message){
@@ -29,12 +43,7 @@ socket.on('newLocationMessage',function(message){
     url:message.url
   });
   $('#messages').append(html);
-
-  // var li = $('<li></li>');
-  // var a = $('<a target="_blank">My current location</a>');
-  // li.text(`${message.from} ${message.createdAt}: `);
-  // a.attr('href',message.url);
-  // li.append(a);
+  scrollToBottom();
 });
 
 var messageText = $('[name=message]');
